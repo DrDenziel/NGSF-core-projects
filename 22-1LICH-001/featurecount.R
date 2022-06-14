0c8918aba2b71a376261ca498d588752f2b001da
DrDenziel <104465157+DrDenziel@users.noreply.github.com>
2022-06-14 11:17:08 -0600

    Create featurecount.R

---
+++ 22-1LICH-001/featurecount.R
@@ -0,0 +1,35 @@
+library(Rsubread)
+library(magrittr)
+setwd("/datastore/NGSF001/projects/22-1LICH-001/daniel_analysis/")
+result_dir="daniel_alignment"
+sample_names <- list.files(result_dir, "R2")
+
+
+#count features using featureCounts function
+feature_count <- sapply(sample_names, function(x)
+			   featureCounts(files = sprintf('%s/%s/Aligned.sortedByCoord.out.bam',result_dir, x),
+			   annot.ext="/datastore/NGSF001/analysis/references/human/gencode-30/gencode.v30.annotation.gtf",
+			   isGTFAnnotationFile = TRUE,
+			   GTF.attrType.extra  = c('gene_name'),
+			   nthreads = 8, 
+			   isPairedEnd = TRUE), 
+			   simplify = FALSE, 
+			   USE.NAMES = TRUE)
+
+
+#convet list to a dataframe
+#COUNTS
+feature_count1 <- feature_count %>% lapply(function(x) x$counts) %>% 
+	do.call(cbind, .) %>% 
+	magrittr::set_colnames(names(feature_count))
+
+#ANNOTATIONS					  
+feature_annotation <- feature_count %>% lapply(function(x) x$annotation) %>% 
+	do.call(cbind, .) %>% 
+	magrittr::extract(,c(1,7)) %>%
+	magrittr::set_colnames(c('GeneID', 'gene_name'))
+
+feature_count <- cbind(feature_annotation,feature_count1)
+
+save(feature_count, file = 'feature_count.RData', compress = 'xz')
+					
